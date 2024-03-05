d68fe343f3a6c15e05aef92810748f6f93869466
Seven <phoenixpyra7@gmail.com>
2024-03-05 15:27:12 -0600

    added a few of the routes and ideas for the routes I may need.

---
+++ routes/api/friendRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getFriends,
+  getSingleFriend,
+  createFriend,
+  deleteFriend,
+  addTask,
+  removeTask,
+} = require('../../controllers/friendController');
+
+// /api/students
+router.route('/').get(getFriends).post(createFriend);
+
+// /api/students/:studentId
+router.route('/:friendId').get(getSingleFriend).delete(deleteFriend);
+
+// /api/students/:studentId/assignments
+router.route('/:friendId/tasks').post(addTask);
+
+// /api/students/:studentId/assignments/:assignmentId
+router.route('/:friendId/tasks/:friendId').delete(removeTask);
+
+module.exports = router;
